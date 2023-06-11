1a609922921270ea7267720cd42b1f5339da79bf
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-06-11 12:03:39 +0200

    wip main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ mod lexer;
 mod repl;
 
 fn main() {
+    
+    // without repl
     // let input: String = String::from("let x = 5;");
     // let mut lexer: lexer::Lexer = lexer::Lexer::new(input.clone());
     // let tok: token::Token = lexer.next_token();
     // println!("{}", tok.token_type);
     // println!("{}", input);
+    
+    // use repl
     repl::start();
 }
